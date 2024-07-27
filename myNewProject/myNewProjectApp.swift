0ca3b3859a7174f877e11e70cf62d3c39dfbcb2d
geek <geek.geunbon@gmail.com>
2024-07-27 15:09:58 +0900

    Initial Commit

---
+++ myNewProject/myNewProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  myNewProjectApp.swift
+//  myNewProject
+//
+//  Created by geunbon on 7/27/24.
+//
+
+import SwiftUI
+
+@main
+struct myNewProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
